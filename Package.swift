c7bba679dae88622d24e47a18977843835e9a748
Jonathan Guthrie <jono@perfect.org>
2017-03-07 14:12:32 -0500

    fixed package name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // Date: 2017-03-07 18:40:16 +0000
 import PackageDescription
 let package = Package(
-    name: "Perfect-Repeater",
+    name: "PerfectRepeater",
     targets: [],
     dependencies: [
     ]
