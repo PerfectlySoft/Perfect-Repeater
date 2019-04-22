f1d7e3536384f57d3ef0ad793c9b8f0298655ac5
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-04-22 13:09:14 -0400

    Updated for Swift 4 package format

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
+// swift-tools-version:4.1
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-03-07 18:40:16 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectRepeater",
-    targets: [],
-    dependencies: [
-    ]
+	products: [.library(name: "PerfectRepeater", targets: ["PerfectRepeater"])],
+	dependencies: [],
+	targets: [.target(name: "PerfectRepeater", dependencies: [])]
 )
